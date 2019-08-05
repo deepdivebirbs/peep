6fff194d67d29c0a43ce2b58a80caf59d7438147
Daniel Juarez <djuarez11@cnm.edu>
2019-08-05 14:16:33 -0600

    fixed ddl

--- sql/favoritebirdlist.sql
+++ sql/favoritebirdlist.sql
@@ -3,32 +3,30 @@ CREATE DATABASE peep CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 -- dropping and re-adding tables to prevent errors. this should only be done once, at the beginning of the project!
 
-DROP TABLE IF EXISTS favoriteBirdList;
+DROP TABLE IF EXISTS favorite;
 
 -- Note that the table FavoriteBirdlist contains two foreign keys, each from a different
 -- table that must be created first. These tables are User and BirdSpecies, and the relationship is
-CREATE TABLE favoriteBirdList (
-	birdFavoriteUserId BINARY(16) NOT NULL,
-   birdFavoriteSpeciesCode VARCHAR(6) NOT NULL,
+CREATE TABLE favorite (
+	favoriteSpeciesId BINARY(16) NOT NULL,
+   favoriteUserProfileId BINARY (16) NOT NULL,
 		-- creates index before making foreign keys--
-	INDEX (birdFavoriteUserId),
-	INDEX (birdFavoriteSpeciesCode),
+	INDEX (favoriteSpeciesId),
+	INDEX (favoriteUserProfileId),
 	-- creates foreign key relations
-	FOREIGN KEY (birdFavoriteUserId) REFERENCES userProfile(userId),
-	FOREIGN KEY (birdFavoriteSpeciesCode) REFERENCES birdSpecies(speciesCode),
+	FOREIGN KEY (favoriteSpeciesId) REFERENCES species(speciesId),
+	FOREIGN KEY (favoriteUserProfileId) REFERENCES userProfile(userProfileId),
 	-- creates a  composite foreign key with the two foreign keys that depend on speciesId and userID
-	PRIMARY KEY (birdFavoriteSpeciesCode, birdFavoriteUserId)
+	PRIMARY KEY (favoriteSpeciesId, favoriteUserProfileId)
  );
 
-DROP TABLE IF EXISTS favoriteBirdList;
-
-
-CREATE TABLE favoriteBirdList (
-	birdFavoriteUserId BINARY(16) NOT NULL,
-	birdFavoriteSpeciesCode VARCHAR(6) NOT NULL,
-	INDEX (birdFavoriteUserId),
-	INDEX (birdFavoriteSpeciesCode),
-	FOREIGN KEY (birdFavoriteUserId) REFERENCES userProfile(userId),
-	FOREIGN KEY (birdFavoriteSpdeciesCode) REFERENCES birdSpecies(speciesCode),
-	PRIMARY KEY (birdFavoriteSpeciesCode, birdFavoriteUserId)
-);
+DROP TABLE IF EXISTS favorite;
+CREATE TABLE favorite (
+	favoriteSpeciesId BINARY(16) NOT NULL,
+   favoriteUserProfileId BINARY (16) NOT NULL,
+	INDEX (favoriteSpeciesId),
+	INDEX (favoriteUserProfileId),
+	FOREIGN KEY (favoriteSpeciesId) REFERENCES species(speciesId),
+	FOREIGN KEY (favoriteUserProfileId) REFERENCES userProfile(userProfileId),
+	PRIMARY KEY (favoriteSpeciesId, favoriteUserProfileId)
+ );
