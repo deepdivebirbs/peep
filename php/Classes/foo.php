fd2a87f6349e08d1e0e0a6653d820a733167a372
Daniel Juarez <djuarez11@cnm.edu>
2019-08-12 13:34:08 -0600

    IDK

---
+++ php/Classes/foo.php
@@ -0,0 +1,8 @@
+<?php
+namespace Birbs\Peep;
+
+require_once "Favorite.php";
+
+$canary = new Favorite('788f9a32-ec96-40ce-b69a-898aedfcd1c8', '2bc9645d-5554-40d7-a0a0-9c89806261f2');
+
+var_dump ($canary);
