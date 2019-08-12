6881cf3255833b727337c8bc012a1ea8a28ca8cc
Daniel Juarez <djuarez11@cnm.edu>
2019-08-12 09:26:56 -0600

    started work on FavoriteTest

--- php/Classes/Favorite.php
+++ php/Classes/Favorite.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Birbs\Peep;
 
 /**
@@ -7,16 +8,19 @@ namespace Birbs\Peep;
  */
 
 require_once "autoload.php";
-require_once(dirname(__DIR__,1)."/vendor/autoload.php");
+require_once(dirname(__DIR__, 1) . "/vendor/autoload.php");
 
+use http\Exception\BadQueryStringException;
 use Ramsey\Uuid\Uuid;
-class favorite implements \JsonSerializable {
-	// TODO JsonSerializable not implemented, needed to figure out how to do so for UUID stringification or can I remove?
+
+class Favorite implements \JsonSerializable {
 	use ValidateUuid;
+
 	/**
 	 *this is the species code of the bird that the user is adding to their favorites; this is a foreign key
 	 */
 	private $favoriteSpeciesId;
+
 	/**
 	 * this is taken from the user id of the user that is adding the bird to their favorites; this is a foreign key
 	 */
@@ -25,127 +29,160 @@ class favorite implements \JsonSerializable {
 	/**
 	 * constructor for this favorite class
 	 *
-	 * @param string $newFavoriteSpeciesId id of the bird species that the user is saving (foreign key)
-	 * @param string $newFavoriteUserProfileId id of the user who is saving this bird (foreign key)
+	 * @param string|Uuid $newFavoriteSpeciesId id of the bird species that the user is saving (foreign key)
+	 * @param string|Uuid $newFavoriteUserProfileId id of the user who is saving this bird (foreign key)
 	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data is out of range
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception occurs
 	 */
-
 	public function __construct($newFavoriteSpeciesId, $newFavoriteUserProfileId) {
-	try {
-		$this->setNewFavoriteSpeciesId($newFavoriteSpeciesId);
-		$this->setNewFavoriteUserProfileId($newFavoriteUserProfileId);
-	}
-		//this is the part where we determine what exception type is thrown, if any
-	catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception)  {
+		try {
+			$this->setNewFavoriteSpeciesId($newFavoriteSpeciesId);
+			$this->setNewFavoriteUserProfileId($newFavoriteUserProfileId);
+		} //this is the part where we determine what exception type is thrown, if any
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw (new $exceptionType ($exception->getMessage(), 0, $exception));
 		}
 	}
+
 	/**
 	 * accessor method for FavoriteSpeciesId
-	 * @return string value for FavoriteSpeciesId
+	 * @return Uuid value for FavoriteSpeciesId
 	 */
-	public function getFavoriteSpeciesId(): string {
-		return($this->getFavoriteSpeciesId());
+	public function getFavoriteSpeciesId(): Uuid {
+		return ($this->favoriteSpeciesId);
+	}
+
+	/**
+	 * accessor method for favoriteUserProfileId
+	 * @return Uuid value of favoriteUserProfileId
+	 */
+	public function getFavoriteUserProfileId(): Uuid {
+		return ($this->favoriteUserProfileId);
 	}
 
 	/**
 	 * mutator method for FavoriteSpeciesId
-	 * @param string $newFavoriteSpeciesId
+	 * @param string|Uuid $newFavoriteSpeciesId
 	 * @throws \InvalidArgumentException if $newFavoriteBirdSpeciesId is empty or insecure
-	 * @throws \RangeException if $newFavoriteSpeciesId is not 6 characters
-	 * @throws \TypeError if $newFavoriteSpeciesId is not a string
+	 * @throws \RangeException if $newFavoriteSpeciesId is out of range
+	 * @throws \TypeError if $newFavoriteSpeciesId is not a Uuid
+	 * @throws \Exception if there is another exception
 	 */
-
-	public function setNewFavoriteSpeciesId ($newFavoriteSpeciesId): void {
+	public function setNewFavoriteSpeciesId($newFavoriteSpeciesId): void {
 		try {
 			$uuid = self::validateUuid($newFavoriteSpeciesId);
 		} catch(\invalidArgumentException |\RangeException|\Exception|\TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new$exceptionType($exception->getMessage(), 0, $exception));
 		}
-		$this->favoriteSpeciesIds = $uuid;
+		$this->favoriteSpeciesId = $uuid;
 	}
 
 	/**
-	 * accessor method for favoriteUserProfileId
-	 * @return string uuid value of favoriteUserProfileId
-	 */
-	public function getFavoriteUserProfileId():Uuid {
-		return ($this->favoriteUserProfileId);
-	}
-	/**
 	 * mutator method for favoriteUserProfileId
 	 * @param Uuid|string $newFavoriteUserProfileId
-	 * $throws \TypeError if $newFavoriteUserProfileId is not a Uuid or string
+	 * @throws \InvalidArgumentException if $newUserProfileId is empty or insecure
+	 * @throws \RangeException if $newFavoriteUserProfileId is out of range
+	 * @throws \TypeError if $newFavoriteUserProfileId is not a Uuid or string
+	 * @throws \Exception if there is another exception
 	 */
-	public function setNewFavoriteUserProfileId ($newFavoriteUserProfileId): void {
-	try {
+	public function setNewFavoriteUserProfileId($newFavoriteUserProfileId): void {
+		try {
 			$uuid = self::validateUuid($newFavoriteUserProfileId);
-		} catch (\invalidArgumentException |\RangeException|\Exception|\TypeError $exception){
-			$exceptionType=get_class($exception);
-		throw(new$exceptionType($exception->getMessage(),0,$exception));
+		} catch(\invalidArgumentException |\RangeException|\Exception|\TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new$exceptionType($exception->getMessage(), 0, $exception));
 		}
-		$this->favoriteUserProfileId=$uuid;
+		$this->favoriteUserProfileId = $uuid;
 	}
+
 	/*
 	 * inserts this favorite bird into mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @throws \PDOexception when mySQL related errors occur
-	 * @throwsTypeError if $pdo is not a PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
 	 */
-	public function insert (\PDO $pdo) : void {
+	public function insert(\PDO $pdo): void {
 		//template for query
 		$query = "INSERT INTO favorite(favoriteSpeciesId, favoriteUserProfileId) 
-		VALUES (:favoriteSpeciesId, :FavoriteUserProfileId )";
+		VALUES (:favoriteSpeciesId, :favoriteUserProfileId )";
 		$statement = $pdo->prepare($query);
 
 		//binds member variables to the placeholders in the template. getbytes converts string into bytes
-		$parameters= [ "favoriteSpeciesId"=>$this->favoriteSpeciesId->getBytes(),
-			"favoriteUserProfileId"=>$this->favoriteUserProfileId->getBytes()];
+		$parameters = ["favoriteSpeciesId" => $this->favoriteSpeciesId->getBytes(),
+			"favoriteUserProfileId" => $this->favoriteUserProfileId->getBytes()];
 		$statement->execute($parameters);
 	}
+
 	/**
 	 * deletes favorite bird from mySQL
+	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 */
-	public function delete (\PDO $pdo) : void {
+	public function delete(\PDO $pdo): void {
+
 		//create query template
-		$query = "DELETE FROM favorite WHERE favoriteUserProfileId=:favoriteSpeciesId";
+		$query = "DELETE FROM favorite WHERE favoriteUserProfileId = :favoriteUserProfileId and = favoriteSpeciesId = :favoriteSpeciesId";
 		$statement = $pdo->prepare($query);
+
 		//binds the member variables to the placeholder in the template
-		$parameters = ["favoriteBirdList"=>$this->getBytes()];
+		$parameters = ["favoriteUserProfileId" => $this->favoriteUserProfileId -> getBytes(), "favoriteSpeciesId" =>
+			$this->favoriteSpeciesId->getBytes()];
 		$statement->execute($parameters);
 	}
 
-	public static function getAllFavoritebyUserProfileId (\PDO $pdo, $favoriteUserProfileId): ?favorite{
+
+	/**
+	 * gets all of a user's favorite birds by userProfileId
+	 * @param \PDO $pdo pdo connection object
+	 * @param $favoriteUserProfileId
+	 * @return favorite bird list
+	 */
+	public static function getAllFavoriteByUserProfileId(\PDO $pdo, string $favoriteUserProfileId): \SplFixedArray {
+		try {
+			$favoriteUserProfileId = self::validateUuid($favoriteUserProfileId);
+		} catch (\InvalidArgumentException |\ RangeException |\Exception |\TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(),0, $exception));
+		}
 		//create query template
-		$query = "SELECT favoriteSpeciesId, favoriteUserProfileId FROM favoriteBirdList WHERE favoriteUserProfileId =:favoriteUserProfileId";
-		$statement=$pdo->prepare($query);
+		$query = "SELECT favoriteSpeciesId, favoriteUserProfileId FROM favorite WHERE favoriteUserProfileId =:favoriteUserProfileId";
+		$statement = $pdo->prepare($query);
+
 		// bind the favoriteUserProfileId to the place holder in the template
-		$parameters = ["favoriteUserProfileId" => $favoriteUserProfileId ->getBytes ()];
-		$statement ->execute($parameters);
+		$parameters = ["favoriteUserProfileId" => $favoriteUserProfileId->getBytes()];
+		$statement->execute($parameters);
+
 		//build an array of favorites
-		$favoriteBirdList = new \SplFixedArray($statement->rowCount());
-		$statement ->setFetchMode(\PDO::FETCH_ASSOC);
-		while (($row=$statement->fetch())!==false) {
+		$favorites = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
 			try {
-				$favorite = new favorite($row ["favoriteSpeciesId"], $row["favoriteUserProfileId"]);
-			} catch (\Exception $exception) {
-				throw (new \PDOException($exception->getMessage(),$exception));
+				$favorite = new Favorite($row ["favoriteSpeciesId"], $row["favoriteUserProfileId"]);
+				$favorites[$favorite->key()]= $favorite;
+				$favorites->next();
+			} catch(\Exception $exception) {
+				//if row couldn't be converted, rethrow it
+				throw (new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
-		return($favorite);
+		return ($favorites);
 	}
 
-	public function jsonSerialize() : array {
-	$fields =get_object_vars ($this);
+	/**
+	 * jsonSerialize converts uuids to string
+	 */
+	public function jsonSerialize(): array {
+		$fields = get_object_vars($this);
 
-	$fields ["favoriteUserProfileId"] = $this->favoriteUserProfileId->toString();
+		$fields ["favoriteUserProfileId"] = $this->favoriteUserProfileId->toString();
+		$fields ["favoriteSpeciesId"] = $this->favoriteSpeciesId->toString();
+
+		return ($fields);
 	}
 }
---
+++ php/Classes/Test/FavoriteTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Birbs\Peep\Test;
+
+use Birbs\Peep\ {
+	Favorite
+};
+
+require_once (dirname(__DIR__) . "/autoload.php");
+
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+class FavoriteTest {
+
+}
