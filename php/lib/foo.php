c16f1509f975001a0bb5be27c9104aa0be17cb49
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-12 10:42:22 -0600

    added ext-json and ext-pdo to the composer.json file

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -9,7 +9,6 @@ require_once (dirname(__DIR__, 1) . "/Classes/Sighting.php");
 
 //new \DateTime("now", new \DateTimeZone("America/Denver"));
 
-$foo = new sighting("6099832b-e521-4d90-92f3-a6f354a90a39", "c8c67ec4-bc09-4de2-83cd-93940ab8e807", "8cd72411-79f1-40fc-b9a0-ad62fef9d447", "pretty bird", "birdimus prettimus", 35.085, -106.649, "08/10/18", "birdphoto.com/birdie");
-
+$foo = new Sighting(newSig, newSightin, newSighting, newSight)
 
 var_dump($foo);
