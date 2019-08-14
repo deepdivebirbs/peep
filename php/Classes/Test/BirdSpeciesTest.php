2d360f616ae4e6f6339b5991c28f098310bf56aa
CodeCanna <fleabass4@gmail.com>
2019-08-13 19:08:04 -0600

    Added and documented all of the variables.

--- php/Classes/Test/BirdSpeciesTest.php
+++ php/Classes/Test/BirdSpeciesTest.php
@@ -14,35 +14,45 @@ class BirdSpeciesTest extends PeepTest {
      * 
      * @var Uuid validSpeciesId
      */
-    protected validSpeciesId;
+    protected validSpeciesId = "2ae7ea43-c430-4e8f-96f1-f10727bc809f";
 
     /**
      * A string that represents the given code to a bird species from the Ebirds API.
      * 
      * @var string validSpeciesCode
      */
-    protected validSpeciesCode;
+    protected validSpeciesCode = "123ABC";
 
     /**
      * A string that is the common non-scientific name of a bird species.
      * 
      * @var string validSpeciesComName
      */
-    protected validSpeciesCode;
+    protected validSpeciesCode = "Pinyon Jay";
 
     /**
      * A string that is the given scientific name for a bird species.
      * 
      * @var string validScientificName
      */
-    protected validScientificName;
+    protected validScientificName = "Gymnorhinus cyanocephalus";
 
     /**
      * A string that is the URL for a bird species photo.
      * 
      * @var string validSpeciesPhotoUrl
      */
-    protected validSpeciesPhotoUrl;
+    protected validSpeciesPhotoUrl = "https://ebird.org/species/pinjay";
+
+    // Do I test each function going down the list?
+
+    /**
+     * Create a dependent set of objects before starting the test.
+     */
+    public function setUp(): void {
+        parent::setUp();
+        
+    }
 
 
 }
