b4613401262aa1b6cfc7f91cc7c0c5a4066bd9d2
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-16 09:00:29 -0600

    Starting on test file

---
+++ php/Classes/Test/UserProfileTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Birbs\Peep\Test;
+use Birbs\Peep\BirdSpecies;
+
+require_once("PeepTest.php");
+require_once (dirname(__DIR__, 1) . "/autoload.php");
+
+/**
+ * Unit test for UserProfile
+ */
+
