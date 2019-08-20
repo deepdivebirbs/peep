22b22e0f0f02ee1f56045c2a20f583a2527ca47c
Daniel Juarez <djuarez11@cnm.edu>
2019-08-20 14:41:27 -0600

    deleted unnecessary foo in preparation for merge

--- php/Classes/foo.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Birbs\Peep;
-
-require_once "Favorite.php";
-
-$canary = new Favorite('788f9a32-ec96-40ce-b69a-898aedfcd1c8', '2bc9645d-5554-40d7-a0a0-9c89806261f2');
-
-var_dump ($canary);
