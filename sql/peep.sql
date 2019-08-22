4839a956d4d017da623de876af4a3cbe29a467b8
Daniel Juarez <djuarez11@cnm.edu>
2019-08-22 17:33:23 -0600

    something

--- sql/peep.sql
+++ sql/peep.sql
@@ -31,7 +31,7 @@ create table sighting (
 	sightingId BINARY (16) NOT NULL,
 	sightingUserProfileId BINARY (16) NOT NULL,
 	sightingSpeciesId BINARY (16) NOT NULL,
-	sightingPhoto VARCHAR (128) NULL,
+	sightingBirdPhoto VARCHAR (128) NULL,
 	sightingDateTime DATETIME (6) NOT NULL,
 	sightingLocX FLOAT (6, 3) NOT NULL,
 	sightingLocY FLOAT (6, 3) NOT NULL,    unique(sightingId),
