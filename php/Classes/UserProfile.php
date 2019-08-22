9d558297a64c2d163040e170e9a43a10ce5b0634
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-22 04:45:45 -0600

    Created Get User by Authentication Token function. It seems that will be needed.

--- php/Classes/UserProfile.php
+++ php/Classes/UserProfile.php
@@ -391,6 +391,32 @@ class UserProfile implements \JsonSerializable {
 	}
 
 
+	public function getUserProfileByAuthenticationToken(\PDO $pdo , string $userProfileAuthenticationToken ): ?userProfile {
+//TODO Trim and filterVar
+		// create query template
+		$query = "SELECT userProfileId, userProfileName, userProfileFirstName, userProfileLastName, userProfileEmail, userProfileAuthenticationToken, userProfileHash FROM userProfile WHERE userProfileAuthenticationToken = :userProfileAuthenticationToken";
+		$pdoStatement = $pdo->prepare($query);
+
+		// bind the user id to the place holder in the template
+		$parameters = ["userProfileAuthenticationToken" => $userProfileAuthenticationToken];
+		$pdoStatement->execute($parameters);
+
+		// grab the statement from mySQL
+		try {
+			$userProfile = null;
+			$pdoStatement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $pdoStatement->fetch();
+			if($row !== false) {
+				$userProfile = new userProfile($row["userProfileId"], $row["userProfileName"], $row["userProfileFirstName"], $row["userProfileLastName"], $row["userProfileEmail"], $row["userProfileAuthenticationToken"], $row["userProfileHash"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($userProfile);
+	}
+
+
 	/**
 	 * formats the state variables for JSON serialization. Might need changing.
 	 *
