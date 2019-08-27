39453a26070f12cbb3c5a254251d305b831ff0bc
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-27 15:12:23 -0600

    Working on userprofile api. Adapting to GET, PUT, POST, and DELETE

--- php/public_html/apis/userprofile/index.php
+++ php/public_html/apis/userprofile/index.php
@@ -64,7 +64,61 @@ try {
 			//	$reply->data = $userProfile;
 			//} I didn't make a way to get all users. For now, I'll see whether something comparable is necessary.
 		//}
-	} else {
+	} //Then comes checking for PUT or POST requests.
+	else if($method === "PUT" || $method === "POST") {
+		// enforce the user has a XSRF token
+		verifyXsrf();
+		//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+		$requestContent = file_get_contents("php://input");
+		// This Line Then decodes the JSON package and stores that result in $requestObject
+		$requestObject = json_decode($requestContent);
+		//make sure profile email is available (required field)
+		if(empty($requestObject->userProfileEmail) === true) {
+			throw(new \InvalidArgumentException ("No email for profile.", 405));
+		}
+		if($method === "PUT") {
+			//determine if we have a PUT request. Process PUT request here
+			// retrieve the profile to update
+			$profile = UserProfile::getUserProfileById($pdo, $userProfileId);
+			if($profile == null) {
+				throw(new RuntimeException("Profile does not exist", 404));
+			}
+			//Here, we must add security measures to prevent hostiles from altering profiles maliciously.
+			//To start, enforce the user is signed in.
+			if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $profile->getUserProfileId()) {
+				throw(new \InvalidArgumentException("You must be Signed In to update your profile.", 403));
+			}
+			// update all attributes
+			$profile->setUserProfileId($requestObject->userPprofileId);
+			$profile->setUserProfileAuthenticationToken($requestObject->userProfileAuthenticationToken);
+			$profile->setUserProfileEmail($requestObject->userProfileEmail);
+			$profile->setUserProfileFirstName($requestObject->userProfileFirstName);
+			$profile->setUserProfileLastName($requestObject->userProfileLastName);
+			$profile->setUserProfileHash($requestObject->userProfileHash);
+			$profile->update($pdo);
+			// update reply
+			$reply->message = "Everything updated";
+		}
+	}//We will need a delete option too.
+	else if($method === "DELETE") {
+		//process DELETE request
+		//enforce that the end user has a XSRF token.
+		verifyXsrf();
+		// retrieve the profile to be deleted
+		$profile = UserProfile::getUserProfileById($pdo, $userProfileId);
+		if($profile === null) {
+			throw(new RuntimeException("Profile Does Not Exist", 404));
+		}
+		//enforce the user is signed in.
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $profile->getUserProfileId()) {
+			throw(new \InvalidArgumentException("You must be Signed In to Delete", 403));
+		}
+		// delete profile
+		$profile->delete($pdo);
+		// update reply
+		$reply->message = "Profile deleted";
+	}
+	else {
 		throw(new \InvalidArgumentException("Invalid HTTP Request...", 418));
 	}
 
