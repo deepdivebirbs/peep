3e898ddc5e16011bf31d53cb5d7148474e248c73
Daniel Juarez <djuarez11@cnm.edu>
2019-08-27 17:01:46 -0600

    added part of the sign-in API

--- php/public_html/apis/sign-in/index.php
+++ php/public_html/apis/sign-in/index.php
@@ -19,3 +19,45 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 //prepare an empty reply
 $reply = new stdClass();
 $reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$secrets = new \secrets ("etc/apache2/capstone-mysql/peep.ini");
+	$pdo = $secrets->getPdoObject ();
+	//determine what HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER ["REQUEST_METHOD"];
+
+	//If the method is post, handle the sign-in logic
+	if($method === "POST") {
+
+		//make sure XSRF token is valid
+		verifyXsrf();
+
+		//process the request content and decode the json object into a PHP object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//check to make sure the password and email fields are not empty
+		if (empty($requestObject->userProfileEmail) === true) {
+			throw (new \InvalidArgumentException("email address not provided", 401))
+		} else {
+			$userProfileEmail
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
