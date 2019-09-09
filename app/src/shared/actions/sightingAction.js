ecd935a7fc739318b99fde79ad491f74cf7ee425
Ruth Cisneros <senoritaruth@gmail.com>
2019-09-09 17:16:51 -0600

    finished sighting action

--- app/src/shared/actions/sightingAction.js
+++ app/src/shared/actions/sightingAction.js
@@ -2,5 +2,5 @@ import {httpConfig} from "../utils/http-config";
 
 export const getSightingsBySightingUserProfileId = (sightingUserProfileId) => async dispatch => {
 	const {data} = await httpConfig(`apis/sighting/${sightingUserProfileId}`);
-	dispatch({type: "GET_PROFILE_BY_PROFILE_ID", payload: data })
+	dispatch({type: "GET_SIGHTINGS_BY_SIGHTING_USER_PROFILE_ID", payload: data })
 };
