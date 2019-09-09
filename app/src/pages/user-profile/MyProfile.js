289e1290b1b0676b868732e143ba3037f7a04cb7
Alistair Gillikin <agillikin1@cnm.edu>
2019-09-09 15:20:29 -0600

    Working on Reducer, actions.

--- app/src/pages/user-profile/MyProfile.js
+++ app/src/pages/user-profile/MyProfile.js
@@ -4,21 +4,31 @@ import {Container} from 'react-bootstrap';
 
 export const MyProfile = () => {
 
-	
+	// grab the profile id from the currently logged in account, or null if not found
+	//const userProfileId = UseJwtProfileId();
+
+	// Return the profile by profileId from the redux store
+	const profile = useSelector(state => (state.profile ? state.profile[0] : []));
+	console.log(profile);
+
+	const dispatch = useDispatch();
+
+	const effects = () => {
+		dispatch(getUserProfileById(match.params.userProfileId));
+	};
+
+	const inputs = [match.params.userProfileId];
+	useEffect(effects, inputs);
 
 	return (
 		<>
 			<Container>
 				<h1>My Profile</h1>
 				<div>
-					<span>Username:</span>
-					<p>Username Placeholder</p>
-					<span>First Name:</span>
-					<p>First Name Placeholder</p>
-					<span>Last Name:</span>
-					<p>Last Name Placeholder</p>
-					<span>Email:</span>
-					<p>Email Placeholder</p>
+					<span>Username: {profile && profile.userProfileName}</span>
+					<span>First Name: {profile && profile.userProfileFirstName}</span>
+					<span>Last Name: {profile && profile.userProfileLastName}</span>
+					<span>Email: {profile && profile.userProfileLastName}</span>
 				</div>
 			</Container>
 		</>
--- app/src/shared/reducers/userProfileReducer.js
+++ app/src/shared/reducers/userProfileReducer.js
@@ -1,6 +1,9 @@
 export default (state = [], action) => {
 	switch (action.type) {
-		//
+		case "GET_PROFILE_BY_PROFILE_ID":
+			return [...state, action.payload];
+		case "GET_PROFILE_BY_PROFILE_EMAIL":
+			return action.payload;
 		default:
 			return state;
 	}
