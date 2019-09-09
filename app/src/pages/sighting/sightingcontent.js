2b1e1acf8ca8288652be60332710d8e289436ca1
Ruth Cisneros <senoritaruth@gmail.com>
2019-09-09 08:28:27 -0600

    updated fields to reflect sighting class

--- app/src/pages/sighting/sightingcontent.js
+++ app/src/pages/sighting/sightingcontent.js
@@ -9,61 +9,47 @@ import {FormDebugger} from "../../shared/components/FormDebugger";
 export const sightingcontent = () => {
 	return (
 		<>
-			<Form>
-				<Form.Row>
-					<Form.Group as={Col} controlId="formGridEmail">
-						<Form.Label>Email</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-					</Form.Group>
+			<Container>
+				<Form>
+					<Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Common Name</Form.Label>
+							<Form.Control type="text" placeholder="Search Common Name"/>
+						</Form.Group>
 
-					<Form.Group as={Col} controlId="formGridPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" />
-					</Form.Group>
-				</Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Scientific Name</Form.Label>
+							<Form.Control type="text" placeholder="Search Scientific Name"/>
+						</Form.Group>
+					</Form.Row>
 
-				<Form.Group controlId="formGridAddress1">
-					<Form.Label>Address</Form.Label>
-					<Form.Control placeholder="1234 Main St" />
-				</Form.Group>
+					<Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Date</Form.Label>
+							<Form.Control placeholder="Date"/>
+						</Form.Group>
 
-				<Form.Group controlId="formGridAddress2">
-					<Form.Label>Address 2</Form.Label>
-					<Form.Control placeholder="Apartment, studio, or floor" />
-				</Form.Group>
+						<Form.Group as={Col}>
+							<Form.Label>Time</Form.Label>
+							<Form.Control placeholder="Time"/>
+						</Form.Group>
+					</Form.Row>
 
-				<Form.Row>
-					<Form.Group as={Col} controlId="formGridCity">
-						<Form.Label>City</Form.Label>
-						<Form.Control />
-					</Form.Group>
+					<Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Location</Form.Label>
+							<Form.Control/>
+						</Form.Group>
+					</Form.Row>
 
-					<Form.Group as={Col} controlId="formGridState">
-						<Form.Label>State</Form.Label>
-						<Form.Control as="select">
-							<option>Choose...</option>
-							<option>...</option>
-						</Form.Control>
-					</Form.Group>
-
-					<Form.Group as={Col} controlId="formGridZip">
-						<Form.Label>Zip</Form.Label>
-						<Form.Control />
-					</Form.Group>
-				</Form.Row>
-
-				<Form.Group id="formGridCheckbox">
-					<Form.Check type="checkbox" label="Check me out" />
-				</Form.Group>
-
-				<Button variant="primary" type="submit">
-					Submit
-				</Button>
-			</Form>
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				</Form>
+			</Container>
 		</>
 	);
 };
-
 
 
 export default sightingcontent;
