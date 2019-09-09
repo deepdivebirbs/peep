1b9f16e162c1744274b024e230d906e86f7afd3a
Ruth Cisneros <senoritaruth@gmail.com>
2019-09-09 17:20:09 -0600

    fixed the favorite action a bit

--- app/src/shared/actions/favoriteAction.js
+++ app/src/shared/actions/favoriteAction.js
@@ -3,5 +3,5 @@ import {httpConfig} from "../utils/http-config";
 export const getAllFavorites = (test) =>async (dispatch) => {
 	const payload = await httpConfig.get ("/apis/favorite");
 
-	dispatch ({type: "getAllFavorites", payload: payload.data});
+	dispatch ({type: "GET_ALL_FAVORITES", payload: payload.data});
 };
