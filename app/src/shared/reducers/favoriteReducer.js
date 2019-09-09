48af576fade1eceec171e82f49adc2d4f63ec733
Daniel Juarez <djuarez11@cnm.edu>
2019-09-09 11:20:17 -0600

    added a favorite action

--- app/src/shared/reducers/favoriteReducer.js
+++ app/src/shared/reducers/favoriteReducer.js
@@ -1,7 +1,8 @@
-import {httpConfig} from "../utils/http-config";
-
-export const getAllFavorites = (test) =>async (dispatch) => {
-	const payload = await httpConfig.get ("/apis/favorite");
-
-	dispatch ({type: "getAllFavorites", payload: payload.data});
-};
+export default (state = [], action) => {
+	switch(action.type) {
+		case "getAllFavorites":
+			return action.payload
+		default:
+			return state;
+	}
+}
