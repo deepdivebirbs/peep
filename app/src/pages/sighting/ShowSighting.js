c3ff5a76b5618216665bb58e3d835c292245f0a7
Ruth Cisneros <senoritaruth@gmail.com>
2019-09-09 16:19:51 -0600

    adding a way to show sightings

--- app/src/pages/sighting/ShowSighting.js
+++ app/src/pages/sighting/ShowSighting.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import httpConfig from '../../../../app/src/shared/utils/http-config';
+import {httpConfig} from '../../../../app/src/shared/utils/http-config'
+import {Container} from 'react-bootstrap';
 
 export const ShowSighting = () => {
+
+	// grab the profile id from the currently logged in account, or null if not found
+	//const userProfileId = UseJwtProfileId();
+
+	// Return the profile by profileId from the redux store
+	const profile = useSelector(state => (state.profile ? state.profile[0] : []));
+	console.log(profile);
+
+	const dispatch = useDispatch();
+
+	const effects = () => {
+		dispatch(getSightingsBySightingUserProfileId(match.params.userProfileId));
+	};
+
+	const inputs = [match.params.userProfileId];
+	useEffect(effects, inputs);
+
 	return (
 		<>
 			<Container>
