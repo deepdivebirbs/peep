6d5e1a2fc20e45fb1db9fc44e1c351e387e6d435
CodeCanna <fleabass4@gmail.com>
2019-09-11 08:51:49 -0600

    Got footer to the bottom of the page.

--- app/src/shared/components/Footer.js
+++ app/src/shared/components/Footer.js
@@ -2,9 +2,21 @@ import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 
 export const Footer = () => {
+	var style = {
+		backgroundColor: "#F8F8F8",
+		borderTop: "1px solid #E7E7E7",
+		textAlign: "center",
+		padding: "20px",
+		position: "fixed",
+		left: "0",
+		bottom: "0",
+		height: "60px",
+		width: "100%",
+	};
+
 	return (
 		<>
-			<footer className="footer">
+			<footer style={style} className="footer">
 				<Row>
 					<Col><a href={"/"}>Home</a></Col>
 					<Col><a href={"/my-profile"}>Profile</a></Col>
