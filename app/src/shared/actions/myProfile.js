b13d90e04977dc2fcee973edc00644136e07a6b6
Ruth Cisneros <senoritaruth@gmail.com>
2019-09-11 14:56:09 -0600

    changed the path for apis from userprofile to userProfile

--- app/src/shared/actions/myProfile.js
+++ app/src/shared/actions/myProfile.js
@@ -1,6 +1,6 @@
 import {httpConfig} from "../utils/http-config";
 
 export const getUserProfileById = (userProfileId) => async dispatch => {
-	const {data} = await httpConfig(`apis/userprofile/${userProfileId}`);
+	const {data} = await httpConfig(`apis/userProfile/${userProfileId}`);
 	dispatch({type: "GET_PROFILE_BY_PROFILE_ID", payload: data })
 };
